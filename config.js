e053c0f96d76e80cf295a104ea1d54fb0e1948d2
QuangCotegg <le.manh.quang@cotegg.vn>
2026-02-10 10:50:44 +0700

    update name

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Đỗ Hương Ly",
+    valentineName: "Hương Ly",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
